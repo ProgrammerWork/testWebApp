d0369768169a200ca3c0c9e71fb161a00c1d4d36
kadmil <victor.suzdalev@gmail.com>
2015-03-18 22:38:01 +0000

    Unnecessary commit

--- src/TestWebApp/Startup.cs
+++ src/TestWebApp/Startup.cs
@@ -8,6 +8,7 @@ namespace TestWebApp
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddMvc();
         }
 
